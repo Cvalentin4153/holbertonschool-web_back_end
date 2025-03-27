b6a924b5cead53905cbc15fde04212da57820859
cvalentin4153 <cvalentin4153@outlook.com>
2025-03-27 15:59:57 -0400

    Task 3 linter fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -14,15 +14,15 @@ export default class Currency {
     return this._code;
   }
 
-  get name() {
-    return this._name;
-  }
-
   set code(code) {
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
     this._code = code;
+  }
+
+  get name() {
+    return this._name;
   }
 
   set name(name) {
