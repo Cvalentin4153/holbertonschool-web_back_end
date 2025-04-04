fd4380a1da08434106f0a7888ace03918db487be
cvalentin4153 <cvalentin4153@outlook.com>
2025-04-04 11:35:02 -0400

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,16 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
-      return {
-        status: result.status,
-        value: result.reason,
-      };
-    }));
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.message,
+    })));
 }
