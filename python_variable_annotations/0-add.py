4fb6f44693ec9c6a801a330d531ff413beda4cb7
cvalentin4153 <cvalentin4153@outlook.com>
2025-04-23 17:50:45 -0400

    Task 0 fix doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to add two numbers.
+
+Example:
+    >>> add(1.5, 2.5)
+    4.0
+"""
 
 def add(a: float, b: float) -> float:
     return a + b
