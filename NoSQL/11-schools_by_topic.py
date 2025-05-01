dafe3587677dfd92af794decc57f291b34667419
cvalentin4153 <cvalentin4153@outlook.com>
2025-05-01 14:46:16 -0400

    Task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,13 +5,10 @@ Module providing a function to find schools by topic.
 
 
 import pymongo
-from typing import List, Dict
 
 
 def schools_by_topic(mongo_collection, topic):
     """
     Returns the list of school documents having a specific topic.
     """
-    query = {"topic": topic}
-    matching_schools = list(mongo_collection.find(query))
-    return matching_schools
+    return list(mongo_collection.find({"topic": topic}))
