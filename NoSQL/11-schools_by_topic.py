e85bb41d14ef8a011f790533d80b7aa4314223fc
cvalentin4153 <cvalentin4153@outlook.com>
2025-05-01 14:43:50 -0400

    Task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -8,8 +8,10 @@ import pymongo
 from typing import List, Dict
 
 
-def schools_by_topic(mongo_collection, topic: str) -> List[Dict]:
+def schools_by_topic(mongo_collection, topic):
     """
     Returns the list of school documents having a specific topic.
     """
-    return list(mongo_collection.find({"topic": topic}))
+    query = {"topic": topic}
+    matching_schools = list(mongo_collection.find(query))
+    return matching_schools
