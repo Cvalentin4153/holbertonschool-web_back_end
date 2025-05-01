31cc5a054d544a2226e8ee4a3bb5a56e35e57b21
cvalentin4153 <cvalentin4153@outlook.com>
2025-05-01 14:32:18 -0400

    Task 11 fix

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
+"""
+Module providing a function to find schools by topic.
+"""
+
 
 import pymongo
 from typing import List
 
+
 def schools_by_topic(mongo_collection, topic: str) -> List[dict]:
+    """Returns the list of school documents having a specific topic.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        topic (str): The topic searched.
+
+    Returns:
+        List[dict]: A list of school documents that contain the specified topic
+        in their 'topics' list.
+    """
+
     return mongo_collection.find({"topic": topic})
