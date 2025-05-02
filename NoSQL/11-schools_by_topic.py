3fcf88d85ff89a7402b525e2c6d76e9a191d8c60
cvalentin4153 <cvalentin4153@outlook.com>
2025-05-02 10:32:39 -0400

    Task 11 fix(topics)

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -11,4 +11,4 @@ def schools_by_topic(mongo_collection, topic):
     """
     Returns the list of school documents having a specific topic.
     """
-    return list(mongo_collection.find({"topic": topic}))
+    return list(mongo_collection.find({"topics": topic}))
